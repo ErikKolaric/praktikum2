55b771893fbc81f849f8340db3b2ee82288fbc00
olgajokic19 <olgajokic@student.um.si>
2023-05-25 11:34:47 +0200

    user change

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Tabs } from "antd";
 import Appointments from "./Appointments";
 import BarberForm from "../BarberForm";
+import moment from "moment/moment";
 
 const Profile = () => {
   const user = JSON.parse(localStorage.getItem("user"));
@@ -13,6 +14,32 @@ const Profile = () => {
         </Tabs.Pane>
         <Tabs.Pane tab="Profile" key="1">
           {user.role === "barber" && <BarberForm />}
+          {user.role !== "barber" && <div
+            className="my-1 bg-white p-1 flex flex-col gap-1"> 
+           <div className=".flex.gap-2">
+            <h4>
+              <b>
+                Name : {user.name}
+              </b>
+            </h4>
+           </div>
+           <div className=".flex.gap-2">
+            <h4>
+              <b>
+                Email : {user.email}
+              </b>
+            </h4>
+           </div>
+           <div className=".flex.gap-2">
+            <h4>
+              <b>
+                Created on : {
+                  moment(user?.createdAt).format("DD-MM-YYYY hh:mm A")
+                }
+              </b>
+            </h4>
+           </div>
+            </div>}
         </Tabs.Pane>
       </Tabs>
     </div>
