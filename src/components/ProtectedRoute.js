fe10c32b489a52ee5d11c09976982ea577a93f32
rebekacep <rebeka.cep@student.um.si>
2023-05-31 13:56:46 +0200

    Added logo

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import logo from '../images/logo.png'
 
 const ProtectedRoute = ({ children }) => {
   const navigate = useNavigate();
@@ -7,40 +8,42 @@ const ProtectedRoute = ({ children }) => {
   return (
     <div className="layout p-2">
       <div className="header bg-white p-2 flex justify-between items-center">
-        <h2 className="cursor-pointer" onClick={() => navigate("/")}>
+        <h2 className="cursor-pointer flex items-center gap-3" onClick={() => navigate("/")}>
+          <img src={logo} alt="Logo"  width="40" height="40"/>
           <strong className="text-primary">BarberShop</strong>
         </h2>
-          <div className="flex items-center gap-3">
-            <div className="flex items-center gap-1">
-              <i className="ri-shield-user-line"></i>
-              <h3
-                className="uppercase cursor-pointer underline"
-                onClick={() => {
-                  if (user.role === "admin") navigate("/admin");
-                  else navigate("/profile");
-                }}
-              >
-                {user?.name}
-              </h3>
-            </div>
-            {!user && 
-              <i
-                className="ri-logout-box-r-line"
-                onClick={() => {
-                  localStorage.removeItem("user");
-                  navigate("/login");
-                }}
-              ></i>}
-            {user && 
-              <i
-                className="ri-logout-box-r-line"
-                onClick={() => {
-                  localStorage.removeItem("user");
-                  navigate("/login");
-                }}
-              ></i>
-            }
+        <div className="flex items-center gap-3">
+          <div className="flex items-center gap-1">
+            <i className="ri-shield-user-line"></i>
+            <h3
+              className="uppercase cursor-pointer underline"
+              onClick={() => {
+                if (user.role === "admin") navigate("/admin");
+                else navigate("/profile");
+              }}
+            >
+              {user?.name}
+            </h3>
           </div>
+          {!user && (
+            <i
+              className="ri-logout-box-r-line"
+              onClick={() => {
+                localStorage.removeItem("user");
+                navigate("/login");
+              }}
+            ></i>
+          )}
+          {user && (
+            <i
+              className="ri-logout-box-r-line"
+              onClick={() => {
+                localStorage.removeItem("user");
+                navigate("/login");
+              }}
+            ></i>
+          )}
+        </div>
       </div>
       <div className="content my-1">{children}</div>
     </div>
