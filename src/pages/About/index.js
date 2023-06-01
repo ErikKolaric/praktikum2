6bbe321cb7db933b7511088fa863bbef241d7522
ErikKolaric <erik.kolaric99@gmail.com>
2023-06-01 08:41:31 +0200

    dodat

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+//HEADER, PARAGRAFI, GOOGLE MAPS LOKACIJA NASA, KONTAKT ITD.
 const About = () => {
     return(<div><h1>ABOUT</h1></div>)
 }
--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// H1, PARAGRAFI, SLIKE(NAJBOLJE SLIDER Z VEC SLIK) ITD.
 const Welcome = () => {
     return(<div><h1>WELCOME</h1></div>)
 }
